85fbf90da59379a006eed0adb51e20eadb2f9607
Hector Remedios <hectoremedio90@gmail.com>
2020-11-13 23:02:01 -0600

    deploy compilation ;)

---
+++ build/precache-manifest.ae784f62c16a34c2fecfafa4cea46242.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2616119ecf91dd8869b9999c6bad463d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb294c84b5d702e0ea0",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "88caaa4ea3ba5d8f80b9",
+    "url": "/static/css/main.adb9212a.chunk.css"
+  },
+  {
+    "revision": "8cb294c84b5d702e0ea0",
+    "url": "/static/js/2.0806973e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0806973e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88caaa4ea3ba5d8f80b9",
+    "url": "/static/js/main.c1d17dc2.chunk.js"
+  },
+  {
+    "revision": "7cbfc43ee168ad8d1c0d",
+    "url": "/static/js/runtime-main.78c1ad32.js"
+  },
+  {
+    "revision": "cb36dd69f110be9b0aa4175a86b9cc8b",
+    "url": "/static/media/CloseBTN.cb36dd69.svg"
+  },
+  {
+    "revision": "020178039c2e4d8b7ffef13349f157ce",
+    "url": "/static/media/Plus.02017803.svg"
+  },
+  {
+    "revision": "8e4e5122728a80f645cbb59c9085148d",
+    "url": "/static/media/ProfileImage.8e4e5122.svg"
+  },
+  {
+    "revision": "f48bd1d082519771061a448aa237e24a",
+    "url": "/static/media/RollingCover.f48bd1d0.jpeg"
+  },
+  {
+    "revision": "4663ed1baad2de755d86395e603fe894",
+    "url": "/static/media/Search.4663ed1b.svg"
+  },
+  {
+    "revision": "68028e00e94005c07ece294cda8c739b",
+    "url": "/static/media/error404.68028e00.svg"
+  },
+  {
+    "revision": "2a1b2c001b49724df50d37277c1c6a42",
+    "url": "/static/media/logo.2a1b2c00.svg"
+  },
+  {
+    "revision": "9770dc49f44a5f201815b79e2d79d52c",
+    "url": "/static/media/logoAnimation.9770dc49.svg"
+  }
+]);
